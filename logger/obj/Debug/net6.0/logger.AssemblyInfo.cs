ec1ffdb01f647ec365b33f8ccee603e37cf7236c
Mushu <ignat-turin@mail.ru>
2024-12-19 15:16:04 +0300

    logger

--- logger/obj/Debug/net6.0/logger.AssemblyInfo.cs
+++ logger/obj/Debug/net6.0/logger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("logger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a3a2cbbbdd88aabed0a1ad4e0f58e3c5b18b6d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc462cc798d3915bb09cd1bc90c365a2385ea71")]
 [assembly: System.Reflection.AssemblyProductAttribute("logger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("logger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
